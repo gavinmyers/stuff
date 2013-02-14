2865469ed56a659f9355655cd772945b28c6bff8
gavin <gavin@crunchbang.(none)>
2013-02-13 22:18:42 -0600

    javax stuff

--- clj/test-cyan.clj
+++
@@ -1,8 +0,0 @@
-(ns cyan-test-2
-  (:use clojure.test))
-
-(defn something [x] 
-  (= true x))
-
-(deftest test-something
-  (is (= false (something false))))
--- clj/test.clj
+++ clj/test.clj
@@ -1,25 +1,48 @@
-(ns cyan-test
-  (:use clojure.test))
+(import javax.imageio.ImageIO)
+(import java.io.File)
+(import javax.swing.JFrame)
+(import javax.swing.JLabel)
+(import javax.swing.JPanel)
+(import javax.swing.ImageIcon)
+(import java.awt.event.ActionEvent)
+(import java.awt.Dimension)
+(import java.awt.Color)
+(import javax.swing.Timer)
+(import java.awt.image.PixelGrabber)
+(import java.awt.event.ActionListener)
+(import java.awt.event.KeyListener)
+(import java.awt.image.BufferedImage)
 
-(defn something [x] 
-  (println x)
-  (= true x))
+(def width 1024)
+(def height 768)
+(def canvas (BufferedImage. width height BufferedImage/TYPE_INT_RGB))
 
-(deftest test-something
-  (is (= true (something true)))
-  (is (= false (something nil)))
-  (is (= false (something false))))
+(defn cyan-panel []
+  (proxy [JPanel ActionListener KeyListener] []
+    (paintComponent [g] 
+      (proxy-super paintComponent g)
+      (. g drawImage canvas nil nil))
+    (actionPerformed [e]
+      (.repaint this))
+    (keyReleased [e])
+    (keyTyped [e])
+    (getPreferredSize [] (Dimension. width height))))
 
-(defn world [] 
-  (println "world"))
+ 
+(defn main []
+  (def panel (cyan-panel))
+  (def timer (Timer. 5000 panel))
+  (def spriteMap (ImageIO/read (File. "1bitcharanim.png")))
+  (def hero (JLabel. (ImageIcon. (.getSubimage spriteMap 8 8 8 8))))
+  (def frame (JFrame. "Goop"))
+  (doto frame
+    (. add panel)
+    (. add hero)
+    (. pack)
+    (. setVisible true)
+    (. setResizable true)
+    (. setSize width height)
+    (. setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE)))
+  (.start timer))
 
-(deftest test-world
-  (world))
-
-(defn wolf []
-  (println "you just made a wolf"))
-
-(defn rabbit []
-  (println "you just made a wabbit"))
-
-(run-all-tests)
+(main)
