04eebba4b86f219a96fd75eec6b3b618434d0bd4
gavin myers <gavin.myers@quantumretail.com>
2013-05-14 12:54:45 -0500

    messing with clj

--- clj/goop.clj
+++ clj/goop.clj
@@ -7,10 +7,28 @@
 (import java.awt.event.ActionListener)
 (import java.awt.event.KeyListener)
 (import java.awt.image.BufferedImage)
-
 (def width 1024)
 (def height 768)
 (def canvas (BufferedImage. width height BufferedImage/TYPE_INT_RGB))
+(def frame (JFrame. "Goop"))
+
+(defn rand-color [] 
+  (int (. (new Color (rand-int 255) (rand-int 255) (rand-int 255)) (getRGB))))
+
+(defn mutate-color [old_color] 
+  (int (rand-int old_color))) 
+
+(defn image-randomize []
+  (int-array (repeatedly (* width height) #(rand-color))))
+
+(defn image-mutate [old]
+  (int-array (for [n (range 0 (* width height))] (mutate-color n)) ))
+
+(defn randomize [^BufferedImage image]
+  (let [w (.getWidth image)
+        h (.getHeight image)
+        out (image-mutate (image-randomize))]
+    (. image setRGB 0 0 w h out 0 w)))
 
 (defn goop-panel []
   (proxy [JPanel ActionListener KeyListener] []
@@ -18,20 +36,16 @@
       (proxy-super paintComponent g)
       (. g drawImage canvas nil nil))
     (actionPerformed [e]
-      (doseq [x (range width) y (range height)]
-        (.setRGB canvas x y (. (new Color (rand-int 255) (rand-int 255) (rand-int 255)) (getRGB))))
+      (randomize canvas) 
       (.repaint this))
     (keyReleased [e])
     (keyTyped [e])
     (getPreferredSize [] (Dimension. width height))))
 
- 
+(def panel (goop-panel))
+
 (defn main []
-  (def panel (goop-panel))
-  (def timer (Timer. 0 panel))
-  (def frame (JFrame. "Goop"))
-  (doseq [x (range width) y (range height)]
-    (.setRGB canvas x y (. (new Color (rand-int 255) (rand-int 255) (rand-int 255)) (getRGB))))
+  (randomize canvas) 
   (doto frame
     (. add panel)
     (. pack)
@@ -39,6 +53,6 @@
     (. setResizable true)
     (. setSize width height)
     (. setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE)))
-  (.start timer))
+  (.start (Timer. 0 panel)))
 
 (main)
