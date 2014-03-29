01088028557822511dd30158dfd0b925099b1d9a
gavin <gavin@gavinm.com>
2014-03-28 23:50:09 -0500

    layout good, will throw divide by zero from the extra cells being drawn

--- igor/gosrc/main.go
+++ igor/gosrc/main.go
@@ -6,6 +6,10 @@ import "math/rand"
 import "strconv"
 import "time"
 
+var WINDOW_WIDTH = 0
+var WINDOW_HEIGHT = 0
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 func print_tb(x, y int, fg, bg termbox.Attribute, msg string) {
   for _, c := range msg {
     termbox.SetCell(x, y, c, fg, bg)
@@ -29,21 +33,43 @@ func main() {
   termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 loop:
   for {
-    printf_tb(2, 20, termbox.ColorCyan, termbox.ColorBlack, "@")
-    printf_tb(4, 20, termbox.ColorCyan, termbox.ColorBlack, "Hello World")
-    termbox.SetCell(0, 0, 0x250C, termbox.ColorWhite, termbox.ColorBlack)
-    termbox.SetCell(1, 1, 0x251C, termbox.ColorWhite, termbox.ColorBlack)
-    termbox.SetCell(2, 2, 0x252C, termbox.ColorWhite, termbox.ColorBlack)
-    termbox.SetCell(2, 3, 0x165C, termbox.ColorWhite, termbox.ColorBlack)
-    termbox.SetCell(4, 5, 0x0298, termbox.ColorWhite, termbox.ColorBlack)
-    r := rand.New(rand.NewSource(time.Now().UnixNano()))
-    height, width := termbox.Size()
-    i := r.Intn(height);
-    i2 := r.Intn(width);
-    printf_tb(3, 23, termbox.ColorCyan, termbox.ColorBlack, strconv.Itoa(i))
-    printf_tb(4, 25, termbox.ColorCyan, termbox.ColorBlack, strconv.Itoa(i2))
-    printf_tb(4, 26, termbox.ColorCyan, termbox.ColorBlack, strconv.Itoa(height))
-    printf_tb(5, 27, termbox.ColorCyan, termbox.ColorBlack, strconv.Itoa(width))
+    WINDOW_WIDTH, WINDOW_HEIGHT = termbox.Size()
+    termbox.Clear(termbox.ColorDefault, termbox.ColorRed)
+    printf_tb((WINDOW_WIDTH / 2) - 8, 0, termbox.ColorCyan, termbox.ColorBlack, "--- I.G.O.R. ---")
+    termbox.SetCell(0, 0, 0x253C, termbox.ColorWhite, termbox.ColorBlack)
+    termbox.SetCell(WINDOW_WIDTH - 1, 0, 0x253C, termbox.ColorWhite, termbox.ColorBlack)
+    termbox.SetCell(WINDOW_WIDTH - 1, WINDOW_HEIGHT - 1, 0x253C, termbox.ColorWhite, termbox.ColorBlack)
+    termbox.SetCell(0, WINDOW_HEIGHT - 1, 0x253C, termbox.ColorWhite, termbox.ColorBlack)
+    printf_tb(0, 1, termbox.ColorCyan, termbox.ColorBlack, strconv.Itoa(WINDOW_WIDTH))
+    printf_tb(0, 2, termbox.ColorCyan, termbox.ColorBlack, strconv.Itoa(WINDOW_HEIGHT))
+    rwidth := r.Intn(WINDOW_WIDTH)
+    rwidth_c1 := r.Intn(rwidth)
+    rwidth_c2 := r.Intn(WINDOW_WIDTH - rwidth) + rwidth
+    rheight := r.Intn(WINDOW_HEIGHT)
+    rheight_c1 := r.Intn(rheight)
+    rheight_c2 := r.Intn(WINDOW_HEIGHT - rheight) + rheight
+    printf_tb(0, 4, termbox.ColorCyan, termbox.ColorBlack, strconv.Itoa(rwidth))
+    printf_tb(0, 3, termbox.ColorCyan, termbox.ColorBlack, strconv.Itoa(rheight))
+    for j := 0; j < WINDOW_WIDTH; j++ {
+      printf_tb(j,rheight,termbox.ColorCyan, termbox.ColorBlack, "-")
+      printf_tb(j,rheight_c1,termbox.ColorCyan, termbox.ColorBlack, "-")
+      printf_tb(j,rheight_c2,termbox.ColorCyan, termbox.ColorBlack, "-")
+    }
+    for i := 0; i < WINDOW_HEIGHT; i++ {
+      printf_tb(rwidth,i,termbox.ColorCyan, termbox.ColorBlack, "|")
+      printf_tb(rwidth_c1,i,termbox.ColorCyan, termbox.ColorBlack, "|")
+      printf_tb(rwidth_c2,i,termbox.ColorCyan, termbox.ColorBlack, "|")
+    }
+    printf_tb(rwidth,rheight,termbox.ColorRed, termbox.ColorBlack, "*")
+    printf_tb(rwidth,rheight-2,termbox.ColorRed, termbox.ColorBlack, "N")
+    printf_tb(rwidth,rheight+2,termbox.ColorRed, termbox.ColorBlack, "S")
+    printf_tb(rwidth+3,rheight,termbox.ColorRed, termbox.ColorBlack, "E")
+    printf_tb(rwidth-3,rheight,termbox.ColorRed, termbox.ColorBlack, "W")
+    printf_tb(rwidth-2,rheight-1,termbox.ColorRed, termbox.ColorBlack, "NW")
+    printf_tb(rwidth+1,rheight-1,termbox.ColorRed, termbox.ColorBlack, "NE")
+    printf_tb(rwidth-2,rheight+1,termbox.ColorRed, termbox.ColorBlack, "SW")
+    printf_tb(rwidth+1,rheight+1,termbox.ColorRed, termbox.ColorBlack, "SE")
+
     termbox.Flush()
     ev := termbox.PollEvent()
     if(ev.Key == termbox.KeyCtrlC) {
