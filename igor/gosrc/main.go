44a35dd3f49a9084e201387412409f9d7d0619e7
gavin <gavin@gavinm.com>
2014-03-30 22:43:34 -0500

    cleanup

--- igor/gosrc/main.go
+++ igor/gosrc/main.go
@@ -11,27 +11,18 @@ var WINDOW_HEIGHT = 0
 var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 var COLOR [256]termbox.Attribute
 
-func print_tb(x, y int, fg, bg termbox.Attribute, msg string) {
-	for _, c := range msg {
-		termbox.SetCell(x, y, c, fg, bg)
-		x++
-	}
-}
-
-func printf_tb(x, y int, fg, bg termbox.Attribute, format string, args ...interface{}) {
-	s := fmt.Sprintf(format, args...)
-	print_tb(x, y, fg, bg, s)
-}
-
-func main() {
+func init() {
 	err := termbox.Init()
 	termbox.SetColorMode(termbox.ColorMode256)
-  for i := 0; i < 255; i++ {
-    COLOR[i] = termbox.Attribute(i)
-  }
 	if err != nil {
 		panic(err)
 	}
+  for i := 0; i < 255; i++ {
+    COLOR[i] = termbox.Attribute(i)
+  }
+}
+
+func main() {
 	defer termbox.Close()
 	termbox.SetInputMode(termbox.InputEsc)
 	termbox.Clear(COLOR[0], COLOR[255])
@@ -39,19 +30,27 @@ loop:
 	for {
 		WINDOW_WIDTH, WINDOW_HEIGHT = termbox.Size()
 		termbox.Clear(COLOR[0], COLOR[rand.Intn(len(COLOR))])
-		printf_tb((WINDOW_WIDTH/2)-8, 0, COLOR[32], COLOR[0], "--- I.G.O.R. ---")
+
 		termbox.SetCell(0, 0, 0x253C, COLOR[255], COLOR[0])
 		termbox.SetCell(WINDOW_WIDTH-1, 0, 0x253C, COLOR[255], COLOR[0])
 		termbox.SetCell(WINDOW_WIDTH-1, WINDOW_HEIGHT-1, 0x253C, COLOR[255], COLOR[0])
 		termbox.SetCell(0, WINDOW_HEIGHT-1, 0x253C, COLOR[255], COLOR[0])
+
+
+		rwidth := r.Intn(WINDOW_WIDTH/2) + WINDOW_WIDTH/4
+
+		rwidth_c1 := r.Intn(rwidth/2) + rwidth/4
+		rwidth_c2 := r.Intn((WINDOW_WIDTH-rwidth)/2) +
+                 rwidth + ((WINDOW_WIDTH - rwidth) / 4)
+
+		rheight := r.Intn(WINDOW_HEIGHT/2) + WINDOW_HEIGHT/4
+
+		rheight_c1 := r.Intn(rheight/2) + rheight/4
+		rheight_c2 := r.Intn((WINDOW_HEIGHT-rheight)/2) +
+                  rheight + ((WINDOW_HEIGHT - rheight) / 4)
+
 		printf_tb(0, 1, COLOR[32], COLOR[0], strconv.Itoa(WINDOW_WIDTH))
 		printf_tb(0, 2, COLOR[32], COLOR[0], strconv.Itoa(WINDOW_HEIGHT))
-		rwidth := r.Intn(WINDOW_WIDTH/2) + WINDOW_WIDTH/4
-		rwidth_c1 := r.Intn(rwidth/2) + rwidth/4
-		rwidth_c2 := r.Intn((WINDOW_WIDTH-rwidth)/2) + rwidth + ((WINDOW_WIDTH - rwidth) / 4)
-		rheight := r.Intn(WINDOW_HEIGHT/2) + WINDOW_HEIGHT/4
-		rheight_c1 := r.Intn(rheight/2) + rheight/4
-		rheight_c2 := r.Intn((WINDOW_HEIGHT-rheight)/2) + rheight + ((WINDOW_HEIGHT - rheight) / 4)
 		printf_tb(0, 4, COLOR[32], COLOR[0], strconv.Itoa(rwidth))
 		printf_tb(0, 3, COLOR[32], COLOR[0], strconv.Itoa(rheight))
 
@@ -67,30 +66,37 @@ loop:
 		printf_tb(rwidth_c2+1, rheight-1, COLOR[255], COLOR[0], ".")
 		printf_tb(WINDOW_WIDTH-1, rheight-1, COLOR[255], COLOR[0], ".")
 
-		printf_tb(0, rheight_c2-1, termbox.Attribute(rand.Int()*255), COLOR[0], "*")
+		printf_tb(0, rheight_c2-1, COLOR[rand.Intn(len(COLOR))], COLOR[0], "*")
+
 		currentWidth := 0
 		currentHeight := rheight
+
 		for i := 0; i < WINDOW_WIDTH; i++ {
-			printf_tb(currentWidth, currentHeight, termbox.Attribute(rand.Int()*255), COLOR[0], "*")
-			if r.Intn(3) == 1 {
+      termbox.SetCell(currentWidth,
+                      currentHeight,
+                      0x00A4,
+                      COLOR[rand.Intn(len(COLOR))],
+                      COLOR[rand.Intn(len(COLOR))])
+			if r.Intn(2) == 1 {
 				currentHeight--
-			} else if r.Intn(3) == 1 {
-				currentHeight++
 			} else {
 				currentHeight++
 			}
-			if r.Intn(3) == 1 {
-				currentWidth++
-			} else if r.Intn(3) == 1 {
-				currentWidth++
+			if r.Intn(2) == 1 {
+				currentWidth--
 			} else {
 				currentWidth++
 			}
 		}
+
 		currentWidth = 0
 		currentHeight = rheight
 		for i := 0; i < WINDOW_WIDTH; i++ {
-			printf_tb(currentWidth, currentHeight, termbox.Attribute(rand.Int()*255), COLOR[0], "*")
+      termbox.SetCell(currentWidth,
+                      currentHeight,
+                      0x00A4,
+                      COLOR[rand.Intn(len(COLOR))],
+                      COLOR[rand.Intn(len(COLOR))])
 			if r.Intn(3) == 1 {
 				currentHeight--
 			} else if r.Intn(3) == 1 {
@@ -123,11 +129,13 @@ loop:
 			printf_tb(j, rheight_c1, COLOR[16], COLOR[0], "-")
 			printf_tb(j, rheight_c2, COLOR[16], COLOR[0], "-")
 		}
+
 		for i := 0; i < WINDOW_HEIGHT; i++ {
 			printf_tb(rwidth, i, COLOR[16], COLOR[0], "|")
 			printf_tb(rwidth_c1, i, COLOR[16], COLOR[0], "|")
 			printf_tb(rwidth_c2, i, COLOR[16], COLOR[0], "|")
 		}
+
 		printf_tb(rwidth, rheight, COLOR[120], COLOR[0], "*")
 		printf_tb(rwidth, rheight-2, COLOR[120], COLOR[0], "N")
 		printf_tb(rwidth, rheight+2, COLOR[120], COLOR[0], "S")
@@ -138,6 +146,7 @@ loop:
 		printf_tb(rwidth-2, rheight+1, COLOR[120], COLOR[0], "SW")
 		printf_tb(rwidth+1, rheight+1, COLOR[120], COLOR[0], "SE")
 
+		printf_tb((WINDOW_WIDTH/2)-8, 0, COLOR[32], COLOR[0], "--- I.G.O.R. ---")
 		termbox.Flush()
 		ev := termbox.PollEvent()
 		if ev.Key == termbox.KeyCtrlC {
@@ -145,3 +154,17 @@ loop:
 		}
 	}
 }
+
+func print_tb(x, y int, fg, bg termbox.Attribute, msg string) {
+	for _, c := range msg {
+		termbox.SetCell(x, y, c, fg, bg)
+		x++
+	}
+}
+
+func printf_tb(x, y int, fg, bg termbox.Attribute, format string, args ...interface{}) {
+	s := fmt.Sprintf(format, args...)
+	print_tb(x, y, fg, bg, s)
+}
+
+
