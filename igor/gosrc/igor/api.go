ad5b4e0d15f020c66e6f1e60e7ca3ea071e8d938
gavin <gavin@gavinm.com>
2014-04-07 21:35:26 -0500

    cleanup

--- igor/gosrc/igor/api.go
+++ igor/gosrc/igor/api.go
@@ -13,35 +13,32 @@ func Connect(startX, startY, endX, endY int, m *Map) {
 	lenT := lenX + lenY
 	path := make([]int, lenT, lenT)
 	for i := 0; i < lenT; i++ {
-    if(startY > endY) {
-      path[i] = DirNorth
+    if(i < lenX) {
+      if(startX > startY) {
+        path[i] = DirWest
+      } else {
+        path[i] = DirEast
+      }
     } else {
-      path[i] = DirSouth
-    }
-  }
-	for i := 0; i < lenX; i++ {
-    if(startX > startY) {
-      path[i] = DirWest
-    } else {
-      path[i] = DirEast
+      if(startY > endY) {
+        path[i] = DirNorth
+      } else {
+        path[i] = DirSouth
+      }
     }
 	}
-  dest := make([]int, len(path))
   perm := rand.Perm(len(path))
-  for i, v := range perm {
-    dest[v] = path[i]
-  }
   tiles := make([]*Tile, lenT)
   pathX := startX
   pathY := startY
-  for i := 0; i < len(dest); i++ {
-    if dest[i] == DirWest {
+  for i, v := range perm {
+    if path[v] == DirWest {
       pathX--
-    } else if dest[i] == DirEast {
+    } else if path[v] == DirEast {
       pathX++
-    } else if dest[i] == DirNorth {
+    } else if path[v] == DirNorth {
       pathY--
-    } else if dest[i] == DirSouth {
+    } else if path[v] == DirSouth {
       pathY++
     }
     t := &Tile {X:pathX, Y:pathY, I:"."}
