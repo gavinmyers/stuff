ad60a60a3228b114f970d117c938cc1de1f61326
gavin <gavin@gavinm.com>
2014-04-07 22:14:38 -0500

    refactoring

---
+++ igor/gosrc/igor/common.go
@@ -0,0 +1,26 @@
+package igor
+
+var WinWidth = 0
+var WinHeight = 0
+var DirNorth = 8
+var DirSouth = 2
+var DirWest = 4
+var DirEast = 6
+var DirNorthWest = 7
+var DirNorthEast = 9
+var DirSouthWest = 1
+var DirSouthEast = 3
+
+type Tile struct {
+  Z int
+  X int
+  Y int
+  I string
+}
+
+
+type Map struct {
+  Z int
+  Tiles [][]*Tile
+}
+
