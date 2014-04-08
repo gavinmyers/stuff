e83adef0b8f3156e2fca77488a60e84f22b29849
gavin <gavin@gavinm.com>
2014-04-07 20:32:01 -0500

    refactoring

--- igor/gosrc/igor/api.go
+++ igor/gosrc/igor/api.go
@@ -1,15 +1,54 @@
 package igor
+import "math/rand"
 
-type Tile struct {
-  Z int
-  X int
-  Y int
-  I string
-}
-
-
-type Map struct {
-  Z int
-  Tiles [][]*Tile
+func Connect(startX, startY, endX, endY int, m *Map) {
+	lenX := startX - endX
+	if lenX < 0 {
+		lenX = endX - startX
+	}
+	lenY := startY - endY
+	if lenY < 0 {
+		lenY = endY - startY
+	}
+	lenT := lenX + lenY
+	path := make([]int, lenT, lenT)
+	for i := 0; i < lenT; i++ {
+    if(startY > endY) {
+      path[i] = DIR_NORTH
+    } else {
+      path[i] = DIR_SOUTH
+    }
+  }
+	for i := 0; i < lenX; i++ {
+    if(startX > startY) {
+      path[i] = DIR_WEST
+    } else {
+      path[i] = DIR_EAST
+    }
+	}
+  dest := make([]int, len(path))
+  perm := rand.Perm(len(path))
+  for i, v := range perm {
+    dest[v] = path[i]
+  }
+  tiles := make([]*Tile, lenT)
+  pathX := startX
+  pathY := startY
+  for i := 0; i < len(dest); i++ {
+    if dest[i] == DIR_WEST {
+      pathX--
+    } else if dest[i] == DIR_EAST {
+      pathX++
+    } else if dest[i] == DIR_NORTH {
+      pathY--
+    } else if dest[i] == DIR_SOUTH {
+      pathY++
+    }
+    t := &Tile {X:pathX, Y:pathY, I:"."}
+    tiles[i] = t
+    if(pathX > 0 && pathY > 0) {
+      m.Tiles[pathX][pathY] = t
+    }
+  }
 }
 
--- igor/gosrc/main.go
+++ igor/gosrc/main.go
@@ -6,16 +6,6 @@ import "./igor"
 import "math/rand"
 import "strconv"
 
-var WINDOW_WIDTH = 0
-var WINDOW_HEIGHT = 0
-var DIR_NORTH = 8
-var DIR_SOUTH = 2
-var DIR_WEST = 4
-var DIR_EAST = 6
-var DIR_NORTH_WEST = 7
-var DIR_NORTH_EAST = 9
-var DIR_SOUTH_WEST = 1
-var DIR_SOUTH_EAST = 3
 var COLOR [256]termbox.Attribute
 var MAP *igor.Map
 
@@ -37,11 +27,11 @@ func main() {
 loop:
 	for {
 
-    WINDOW_WIDTH, WINDOW_HEIGHT = termbox.Size()
-    MAP = &igor.Map {Tiles:make([][]*igor.Tile, WINDOW_WIDTH * 2)}
-    for i := 0; i < WINDOW_WIDTH * 2; i++ {
-      MAP.Tiles[i] = make([]*igor.Tile, WINDOW_HEIGHT * 2)
-      for j := 0; j < WINDOW_HEIGHT * 2; j++ {
+    igor.WINDOW_WIDTH, igor.WINDOW_HEIGHT = termbox.Size()
+    MAP = &igor.Map {Tiles:make([][]*igor.Tile, igor.WINDOW_WIDTH * 2)}
+    for i := 0; i < igor.WINDOW_WIDTH * 2; i++ {
+      MAP.Tiles[i] = make([]*igor.Tile, igor.WINDOW_HEIGHT * 2)
+      for j := 0; j < igor.WINDOW_HEIGHT * 2; j++ {
         t := &igor.Tile {X:i, Y:j, I: "▒"}
         MAP.Tiles[i][j] = t
       }
@@ -50,21 +40,21 @@ loop:
 		termbox.Clear(COLOR[0], COLOR[rand.Intn(len(COLOR))])
 
 		termbox.SetCell(0, 0, 0x253C, COLOR[255], COLOR[0])
-		termbox.SetCell(WINDOW_WIDTH-1, 0, 0x253C, COLOR[255], COLOR[0])
-		termbox.SetCell(WINDOW_WIDTH-1, WINDOW_HEIGHT-1, 0x253C, COLOR[255], COLOR[0])
-		termbox.SetCell(0, WINDOW_HEIGHT-1, 0x253C, COLOR[255], COLOR[0])
+		termbox.SetCell(igor.WINDOW_WIDTH-1, 0, 0x253C, COLOR[255], COLOR[0])
+		termbox.SetCell(igor.WINDOW_WIDTH-1, igor.WINDOW_HEIGHT-1, 0x253C, COLOR[255], COLOR[0])
+		termbox.SetCell(0, igor.WINDOW_HEIGHT-1, 0x253C, COLOR[255], COLOR[0])
 
-		rwidth := rand.Intn(WINDOW_WIDTH/2) + WINDOW_WIDTH/4
+		rwidth := rand.Intn(igor.WINDOW_WIDTH/2) + igor.WINDOW_WIDTH/4
 
 		rwidth_c1 := rand.Intn(rwidth/2) + rwidth/4
-		rwidth_c2 := rand.Intn((WINDOW_WIDTH-rwidth)/2) +
-			rwidth + ((WINDOW_WIDTH - rwidth) / 4)
+		rwidth_c2 := rand.Intn((igor.WINDOW_WIDTH-rwidth)/2) +
+			rwidth + ((igor.WINDOW_WIDTH - rwidth) / 4)
 
-		rheight := rand.Intn(WINDOW_HEIGHT/2) + WINDOW_HEIGHT/4
+		rheight := rand.Intn(igor.WINDOW_HEIGHT/2) + igor.WINDOW_HEIGHT/4
 
 		rheight_c1 := rand.Intn(rheight/2) + rheight/4
-		rheight_c2 := rand.Intn((WINDOW_HEIGHT-rheight)/2) +
-			rheight + ((WINDOW_HEIGHT - rheight) / 4)
+		rheight_c2 := rand.Intn((igor.WINDOW_HEIGHT-rheight)/2) +
+			rheight + ((igor.WINDOW_HEIGHT - rheight) / 4)
 
     sections := make([][2]int, 25)
     sections[0][0] = 0
@@ -75,7 +65,7 @@ loop:
     sections[2][1] = rheight_c1
     sections[3][0] = rwidth_c2
     sections[3][1] = rheight_c1
-    sections[4][0] = WINDOW_WIDTH
+    sections[4][0] = igor.WINDOW_WIDTH
     sections[4][1] = rheight_c1
     sections[5][0] = 0
     sections[5][1] = rheight
@@ -85,7 +75,7 @@ loop:
     sections[7][1] = rheight
     sections[8][0] = rwidth_c2
     sections[8][1] = rheight
-    sections[9][0] = WINDOW_WIDTH
+    sections[9][0] = igor.WINDOW_WIDTH
     sections[9][1] = rheight
     sections[10][0] = 0
     sections[10][1] = rheight_c2
@@ -95,18 +85,18 @@ loop:
     sections[12][1] = rheight_c2
     sections[13][0] = rwidth_c2
     sections[13][1] = rheight_c2
-    sections[14][0] = WINDOW_WIDTH
+    sections[14][0] = igor.WINDOW_WIDTH
     sections[14][1] = rheight_c2
     sections[15][0] = 0
-    sections[15][1] = WINDOW_HEIGHT
+    sections[15][1] = igor.WINDOW_HEIGHT
     sections[16][0] = rwidth_c1
-    sections[16][1] = WINDOW_HEIGHT
+    sections[16][1] = igor.WINDOW_HEIGHT
     sections[17][0] = rwidth
-    sections[17][1] = WINDOW_HEIGHT
+    sections[17][1] = igor.WINDOW_HEIGHT
     sections[18][0] = rwidth_c2
-    sections[18][1] = WINDOW_HEIGHT
-    sections[19][0] = WINDOW_WIDTH
-    sections[19][1] = WINDOW_HEIGHT
+    sections[18][1] = igor.WINDOW_HEIGHT
+    sections[19][0] = igor.WINDOW_WIDTH
+    sections[19][1] = igor.WINDOW_HEIGHT
     sections[20][0] = 0
     sections[20][1] = 0
     sections[21][0] = rwidth_c1
@@ -115,18 +105,18 @@ loop:
     sections[22][1] = 0
     sections[23][0] = rwidth_c2
     sections[23][1] = 0
-    sections[24][0] = WINDOW_WIDTH
+    sections[24][0] = igor.WINDOW_WIDTH
     sections[24][1] = 0
 
-		printf_tb(0, 1, COLOR[32], COLOR[0], strconv.Itoa(WINDOW_WIDTH))
-		printf_tb(0, 2, COLOR[32], COLOR[0], strconv.Itoa(WINDOW_HEIGHT))
+		printf_tb(0, 1, COLOR[32], COLOR[0], strconv.Itoa(igor.WINDOW_WIDTH))
+		printf_tb(0, 2, COLOR[32], COLOR[0], strconv.Itoa(igor.WINDOW_HEIGHT))
 		printf_tb(0, 4, COLOR[32], COLOR[0], strconv.Itoa(rwidth))
 		printf_tb(0, 3, COLOR[32], COLOR[0], strconv.Itoa(rheight))
 
 		currentWidth := 0
 		currentHeight := rheight
 
-		for i := 0; i < WINDOW_WIDTH; i++ {
+		for i := 0; i < igor.WINDOW_WIDTH; i++ {
 			termbox.SetCell(currentWidth,
 				currentHeight,
 				0x00A4,
@@ -143,13 +133,13 @@ loop:
 				currentWidth++
 			}
 		}
-		for j := 0; j < WINDOW_WIDTH; j++ {
+		for j := 0; j < igor.WINDOW_WIDTH; j++ {
 			printf_tb(j, rheight, COLOR[16], COLOR[0], "-")
 			printf_tb(j, rheight_c1, COLOR[16], COLOR[0], "-")
 			printf_tb(j, rheight_c2, COLOR[16], COLOR[0], "-")
 		}
 
-		for i := 0; i < WINDOW_HEIGHT; i++ {
+		for i := 0; i < igor.WINDOW_HEIGHT; i++ {
 			printf_tb(rwidth, i, COLOR[16], COLOR[0], "|")
 			printf_tb(rwidth_c1, i, COLOR[16], COLOR[0], "|")
 			printf_tb(rwidth_c2, i, COLOR[16], COLOR[0], "|")
@@ -159,25 +149,25 @@ loop:
 		printf_tb(rwidth_c1+1, rheight_c1-1, COLOR[255], COLOR[0], "02")
 		printf_tb(rwidth+1, rheight_c1-1, COLOR[255], COLOR[0], "03")
 		printf_tb(rwidth_c2+1, rheight_c1-1, COLOR[255], COLOR[0], "04")
-		printf_tb(WINDOW_WIDTH-2, rheight_c1-1, COLOR[255], COLOR[0], "05")
+		printf_tb(igor.WINDOW_WIDTH-2, rheight_c1-1, COLOR[255], COLOR[0], "05")
 
 		printf_tb(0, rheight-1, COLOR[255], COLOR[0], "06")
 		printf_tb(rwidth_c1+1, rheight-1, COLOR[255], COLOR[0], "07")
 		printf_tb(rwidth+1, rheight-1, COLOR[255], COLOR[0], "08")
 		printf_tb(rwidth_c2+1, rheight-1, COLOR[255], COLOR[0], "09")
-		printf_tb(WINDOW_WIDTH-2, rheight-1, COLOR[255], COLOR[0], "10")
+		printf_tb(igor.WINDOW_WIDTH-2, rheight-1, COLOR[255], COLOR[0], "10")
 
 		printf_tb(0, rheight_c2-1, COLOR[255], COLOR[0], "11")
 		printf_tb(rwidth_c1+1, rheight_c2-1, COLOR[255], COLOR[0], "12")
 		printf_tb(rwidth+1, rheight_c2-1, COLOR[255], COLOR[0], "13")
 		printf_tb(rwidth_c2+1, rheight_c2-1, COLOR[255], COLOR[0], "14")
-		printf_tb(WINDOW_WIDTH-2, rheight_c2-1, COLOR[255], COLOR[0], "15")
+		printf_tb(igor.WINDOW_WIDTH-2, rheight_c2-1, COLOR[255], COLOR[0], "15")
 
-		printf_tb(0, WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "16")
-		printf_tb(rwidth_c1+1, WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "17")
-		printf_tb(rwidth+1, WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "18")
-		printf_tb(rwidth_c2+1, WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "19")
-		printf_tb(WINDOW_WIDTH-2, WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "20")
+		printf_tb(0, igor.WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "16")
+		printf_tb(rwidth_c1+1, igor.WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "17")
+		printf_tb(rwidth+1, igor.WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "18")
+		printf_tb(rwidth_c2+1, igor.WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "19")
+		printf_tb(igor.WINDOW_WIDTH-2, igor.WINDOW_HEIGHT-1, COLOR[255], COLOR[0], "20")
 
 		printf_tb(rwidth, rheight, COLOR[120], COLOR[0], "*")
 		printf_tb(rwidth, rheight-2, COLOR[120], COLOR[0], "N")
@@ -192,13 +182,10 @@ loop:
     for i := 0; i < 99; i++ {
       s1 := sections[rand.Intn(len(sections))]
       s2 := sections[rand.Intn(len(sections))]
-      connect(s1[0], s1[1], s2[0], s2[1])
-      //for i := 0; i < len(path); i++ {
-      //  printf_tb(path[i].x, path[i].y, COLOR[120], COLOR[0], ".")
-      //}
+      igor.Connect(s1[0], s1[1], s2[0], s2[1], MAP)
     }
 
-		printf_tb((WINDOW_WIDTH/2)-8, 0, COLOR[32], COLOR[0], "--- I.G.O.R. ---")
+		printf_tb((igor.WINDOW_WIDTH/2)-8, 0, COLOR[32], COLOR[0], "--- I.G.O.R. ---")
     for x := 0; x < len(MAP.Tiles); x++ {
       row := MAP.Tiles[x]
       for y := 0; y < len(MAP.Tiles[x]); y++ {
@@ -214,57 +201,6 @@ loop:
 	}
 }
 
-func connect(startX, startY, endX, endY int) []*igor.Tile {
-	lenX := startX - endX
-	if lenX < 0 {
-		lenX = endX - startX
-	}
-	lenY := startY - endY
-	if lenY < 0 {
-		lenY = endY - startY
-	}
-	lenT := lenX + lenY
-	path := make([]int, lenT, lenT)
-	for i := 0; i < lenT; i++ {
-    if(startY > endY) {
-      path[i] = DIR_NORTH
-    } else {
-      path[i] = DIR_SOUTH
-    }
-  }
-	for i := 0; i < lenX; i++ {
-    if(startX > startY) {
-      path[i] = DIR_WEST
-    } else {
-      path[i] = DIR_EAST
-    }
-	}
-  dest := make([]int, len(path))
-  perm := rand.Perm(len(path))
-  for i, v := range perm {
-    dest[v] = path[i]
-  }
-  tiles := make([]*igor.Tile, lenT)
-  pathX := startX
-  pathY := startY
-  for i := 0; i < len(dest); i++ {
-    if dest[i] == DIR_WEST {
-      pathX--
-    } else if dest[i] == DIR_EAST {
-      pathX++
-    } else if dest[i] == DIR_NORTH {
-      pathY--
-    } else if dest[i] == DIR_SOUTH {
-      pathY++
-    }
-    t := &igor.Tile {X:pathX, Y:pathY, I:"."}
-    tiles[i] = t
-    if(pathX > 0 && pathY > 0) {
-      MAP.Tiles[pathX][pathY] = t
-    }
-  }
-	return tiles
-}
 
 func print_tb(x, y int, fg, bg termbox.Attribute, msg string) {
 	for _, c := range msg {
