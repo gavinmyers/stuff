010a3b17e9354f5ea6e0c22104c672df6cd60a6d
gavin myers <gavin.myers@quantumretail.com>
2014-04-13 21:16:08 -0500

    IT MOVES!

--- igor/gosrc/example/painting.go
+++ igor/gosrc/example/painting.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"gopkg.in/qml.v0"
-	"gopkg.in/qml.v0/gl"
 	"os"
 )
 
@@ -14,43 +13,28 @@ func main() {
 	}
 }
 
-type Tile struct {
+type Player struct {
 	qml.Object
 }
 
-type Sprite struct {
-	qml.Object
+func (r *Player) HandleClick(xPos, yPos int) {
+  r.Set("x",r.Int("x") - 1)
+  r.Set("y",r.Int("y") - 1)
+
 }
 
-func (r *Tile) Paint(p *qml.Painter) {
-	width := gl.Float(r.Int("width"))
-	height := gl.Float(r.Int("height"))
-
-	gl.Enable(gl.BLEND)
-	gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
-	gl.Color4f(1.0, 1.0, 1.0, 1.0)
-	gl.Begin(gl.QUADS)
-	gl.Vertex2f(0, 0)
-	gl.Vertex2f(width, 0)
-	gl.Vertex2f(width, height)
-	gl.Vertex2f(0, height)
-	gl.End()
-
-	gl.LineWidth(0.5)
-	gl.Color4f(1.0, 1.0, 1.0, 1.0)
-	gl.Begin(gl.LINES)
-	gl.Vertex2f(0, 0)
-	gl.Vertex2f(width, height)
-	gl.Vertex2f(width, 0)
-	gl.Vertex2f(0, height)
-	gl.End()
+func (r *Player) Paint(p *qml.Painter) {
+  r.Set("targetX",0)
+  r.Set("x",0)
+  r.Set("targetY",0)
+  r.Set("y",0)
 }
 
 func run() error {
 	qml.Init(nil)
 
 	qml.RegisterTypes("GoExtensions", 1, 0, []qml.TypeSpec{{
-		Init: func(r *Tile, obj qml.Object) { r.Object = obj },
+		Init: func(r *Player, obj qml.Object) { r.Object = obj },
 	}})
 
 	engine := qml.NewEngine()
--- igor/gosrc/example/painting.qml
+++ igor/gosrc/example/painting.qml
@@ -6,17 +6,9 @@ Rectangle {
 
     width: 1260 
     height: 960 
-    color: "blue"
+    color: "gray"
 
-    Tile {
-    	x: 0; y: 0; width: 12; height: 12
-        SequentialAnimation on x {
-            loops: Animation.Infinite
-            NumberAnimation { from: 0; to: 320; duration: 4000; easing.type: Easing.InOutQuad }
-            NumberAnimation { from: 320; to: 0; duration: 4000; easing.type: Easing.InOutQuad }
-        }
-    }
-    Item {
+    Player {
       id: player
       x: 640
       y: 480
@@ -38,6 +30,10 @@ Rectangle {
         x: 200 
         y: 200 
         source: "DawnLike_1/Characters/Player0.png" 
+    }
+    MouseArea {
+        anchors.fill: parent
+        onClicked: player.handleClick(mouse.x, mouse.y)
     }
 
 }
