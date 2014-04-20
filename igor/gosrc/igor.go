d509f07c6d95598ece897b004ff39796e6e180c5
gavin myers <gavin.myers@quantumretail.com>
2014-04-20 00:18:06 -0500

    client needs to be reworked now that it cannot rely on abusing the main thread

--- igor/gosrc/igor.go
+++ igor/gosrc/igor.go
@@ -5,9 +5,12 @@ import ("./moo"
  )
 
 var gui moo.GUI
+var client moo.MooClient
 
 func main() {
   useQml := flag.Bool("gui", true, "use the graphical interface")
+  client = &moo.TelnetMooClient {}
+  client.Init()
   if(*useQml == true) {
     gui = &moo.QmlGUI {}
   } else {
